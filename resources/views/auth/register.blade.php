a0b21de984e5f10ab529d84c554206f7950a00f4
dragonwarriormaster <dragonwarriormaster@outlook.com>
2017-09-26 21:46:26 +0800

    register form

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,104 @@
+{{--@extends('layouts.app')--}}
+
+{{--@section('content')--}}
+{{--<div class="container">--}}
+    {{--<div class="row">--}}
+        {{--<div class="col-md-8 col-md-offset-2">--}}
+            {{--<div class="panel panel-default">--}}
+                {{--<div class="panel-heading">Register</div>--}}
+
+                {{--<div class="panel-body">--}}
+                    {{--<form class="form-horizontal" method="POST" action="{{ route('register') }}">--}}
+                        {{--{{ csrf_field() }}--}}
+
+                        {{--<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">--}}
+                            {{--<label for="name" class="col-md-4 control-label">Name</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>--}}
+
+                                {{--@if ($errors->has('name'))--}}
+                                    {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('name') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
+                            {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>--}}
+
+                                {{--@if ($errors->has('email'))--}}
+                                    {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
+                            {{--<label for="password" class="col-md-4 control-label">Password</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="password" type="password" class="form-control" name="password" required>--}}
+
+                                {{--@if ($errors->has('password'))--}}
+                                    {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('password') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<div class="col-md-6 col-md-offset-4">--}}
+                                {{--<button type="submit" class="btn btn-primary">--}}
+                                    {{--Register--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+{{--@endsection--}}
+
+@extends('layouts.auth')
+
+@section('content')
+    <div class="container">
+        <form class="auth-form mx-auto" action="{{ route('register') }}" method="POST">
+            {{ csrf_field() }}
+
+            <div class="logo display-4 mb-3 text-center text-white">zhihu.</div>
+            <div class="form-group">
+                <input type="text" name="name" class="form-control" value="{{ old('name') }}" placeholder="用户名" required>
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="电子邮箱" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" class="form-control" placeholder="密码" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password-confirmation" class="form-control" placeholder="确认密码" required>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block">注册</button>
+            </div>
+        </form>
+    </div>
+@endsection
