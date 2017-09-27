35317858fcdbe43e6b03695b1fa16cd72d9499a8
dragonwarriormaster <dragonwarriormaster@outlook.com>
2017-09-27 17:54:21 +0800

    reset password form

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,67 @@
+{{--@extends('layouts.app')--}}
+
+{{--@section('content')--}}
+{{--<div class="container">--}}
+    {{--<div class="row">--}}
+        {{--<div class="col-md-8 col-md-offset-2">--}}
+            {{--<div class="panel panel-default">--}}
+                {{--<div class="panel-heading">Reset Password</div>--}}
+
+                {{--<div class="panel-body">--}}
+                    {{--@if (session('status'))--}}
+                        {{--<div class="alert alert-success">--}}
+                            {{--{{ session('status') }}--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
+
+                    {{--<form class="form-horizontal" method="POST" action="{{ route('password.email') }}">--}}
+                        {{--{{ csrf_field() }}--}}
+
+                        {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
+                            {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>--}}
+
+                                {{--@if ($errors->has('email'))--}}
+                                    {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<div class="col-md-6 col-md-offset-4">--}}
+                                {{--<button type="submit" class="btn btn-primary">--}}
+                                    {{--Send Password Reset Link--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+{{--@endsection--}}
+
+@extends('layouts.auth')
+
+@section('content')
+    <div class="container">
+        <form class="auth-form mx-auto" action="{{ route('password.email') }}" method="POST">
+            {{ csrf_field() }}
+
+            <div class="form-group text-white text-center font-weight-bold">
+                <p>重置密码</p>
+            </div>
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block" style="cursor: pointer;">发送密码重置链接</button>
+            </div>
+        </form>
+    </div>
+@endsection
